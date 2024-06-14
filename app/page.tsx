013d7c16dd5a46066a3b639382a840991309fedd
Sugat Ghimire <sugatghimire@gmail.com>
2024-06-14 17:30:49 +0545

    just cleared the public folder inside app folder

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,9 @@ export default function Page() {
             </span>
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"></div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/*Images should be here */}
+        </div>
       </div>
     </main>
   );
